3a32a03bf1cf430e9179fb7a8019e09c9ba4e85e
Arjun Dasgupta <dasgupta1arjun@gmail.com>
2025-04-07 14:39:46 -0400

    forgot to add the return statement for the base cases lol (for solution.py)

--- Recursion-Basic/Unique-3-Digit-Even-Numbers/solution.py
+++ Recursion-Basic/Unique-3-Digit-Even-Numbers/solution.py
@@ -7,7 +7,7 @@ class Solution:
                 if path[0] != 0 and path[2] % 2 == 0:
                     number = path[0] * 100 + path[1] * 10 + path[2]
                     result.add(number)
-                
+                return 
             
             for i in range(len(used)):
                 if used[i]:
