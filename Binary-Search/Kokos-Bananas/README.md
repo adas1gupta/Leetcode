9d33247e0c780d665ee32a3fbc902108a07a676f
adas1gupta <dasgupta1arjun@gmail.com>
2025-11-11 23:44:10 -0500

    solved Koko's Bananas

---
+++ Binary-Search/Kokos-Bananas/README.md
@@ -0,0 +1,16 @@
+# Leetcode 875 - Koko Eating Bananas (Medium)
+
+**Topic**: Binary Search
+**Link**: https://leetcode.com/problems/koko-eating-bananas/description/
+
+## Notes: 
+ - Just run through the problem in your head. 
+ - Play out the scenario end to end. 
+
+
+## 🧪 Code
+See `solution.py`
+
+## ✅ Time & Space
+- Time: O(n) #n is the number of intervals
+- Space: O(1) if result array is not counted as extra space. 
---
+++ Binary-Search/Kokos-Bananas/solution.py
@@ -0,0 +1,19 @@
+import math
+class Solution:
+    def minEatingSpeed(self, piles: List[int], h: int) -> int:
+        left, right = 1, max(piles)
+        
+        while left < right:
+            mid = left + (right - left) // 2
+
+            hours = 0
+
+            for pile in piles:
+                hours += math.ceil(pile / mid)
+
+            if hours > h:
+                left = mid + 1
+            else:
+                right = mid
+        
+        return right
