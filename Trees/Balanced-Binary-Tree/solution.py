8a540feb00212123bc695553550d591518cf04aa
adas1gupta <dasgupta1arjun@gmail.com>
2025-11-18 00:30:17 -0500

    resolved Balanced Binary Tree problem

--- Trees/Balanced-Binary-Tree/solution.py
+++ Trees/Balanced-Binary-Tree/solution.py
@@ -18,3 +18,28 @@ class Solution:
             return [1 + max(left[0], right[0]), (abs(left[0] - right[0]) <= 1) and left[1] and right[1]]
         
         return dfs(root)[1]
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def isBalanced(self, root: Optional[TreeNode]) -> bool:
+        balanced = True
+
+        def dfs(node):
+            nonlocal balanced
+            if not node: return -1
+
+            left = 1 + dfs(node.left)
+            right = 1 + dfs(node.right)
+
+            if abs(left - right) > 1:
+                balanced = False
+            
+            return max(left, right)
+        
+        dfs(root)
+        return balanced
