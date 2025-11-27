85bd4ef8845fb2d8481936726d8ab8d0648e0fec
adas1gupta <dasgupta1arjun@gmail.com>
2025-11-27 00:32:48 -0500

    resolved Clone Graph

--- Graphs/Clone-Graph/solution.py
+++ Graphs/Clone-Graph/solution.py
@@ -6,20 +6,23 @@ class Node:
         self.neighbors = neighbors if neighbors is not None else []
 """
 
+from typing import Optional
 class Solution:
     def cloneGraph(self, node: Optional['Node']) -> Optional['Node']:
-        old_to_new = {}
+        if not node:
+            return node
 
-        def dfs(node):
-            if node in old_to_new:
-                return old_to_new[node]
-            
-            copy = Node(node.val)
-            old_to_new[node] = copy
+        clones = {}
 
-            for neighbor in node.neighbors:
-                copy.neighbors.append(dfs(neighbor))
+        def dfs(root):
+            if root in clones: return clones[root]
+
+            if root not in clones:
+                clones[root] = Node(root.val)
             
-            return copy
+            for neighbor in root.neighbors:
+                clones[root].neighbors.append(dfs(neighbor))
+            
+            return clones[root]
         
-        return dfs(node) if node else None
+        return dfs(node)
