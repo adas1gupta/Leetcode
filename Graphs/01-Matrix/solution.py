2e94629d29c33d8dc6da61e82482ef276324874c
adas1gupta <dasgupta1arjun@gmail.com>
2025-12-09 15:40:13 -0500

    resolved 01 Matrix

--- Graphs/01-Matrix/solution.py
+++ Graphs/01-Matrix/solution.py
@@ -1,34 +1,33 @@
 class Solution:
     def updateMatrix(self, mat: List[List[int]]) -> List[List[int]]:
-        queue = deque([])
         ROWS, COLS = len(mat), len(mat[0])
+        queue = deque([])
         visited = set()
 
         for i in range(ROWS):
             for j in range(COLS):
                 if mat[i][j] == 0:
-                    queue.append((i, j))
-                    visited.add((i, j))
+                    queue.append((i, j, 0))
         
-        def bfs(i, j):
+        def bfs(i, j, dis):
             if i >= ROWS or i < 0: return
             if j >= COLS or j < 0: return
             if (i, j) in visited: return
+            if mat[i][j] == 0: return
 
             visited.add((i, j))
-            queue.append((i, j))
 
-        level = 0
+            mat[i][j] = dis
+
+            queue.append((i, j, dis))
+        
         while queue:
             for _ in range(len(queue)):
-                i, j = queue.popleft()
-                mat[i][j] = level
+                x, y, dist = queue.popleft()
 
-                bfs(i + 1, j)
-                bfs(i - 1, j)
-                bfs(i, j + 1)
-                bfs(i, j - 1)
-
-            level += 1
+                bfs(x + 1, y, dist + 1)
+                bfs(x - 1, y, dist + 1)
+                bfs(x, y + 1, dist + 1)
+                bfs(x, y - 1, dist + 1)
         
         return mat
